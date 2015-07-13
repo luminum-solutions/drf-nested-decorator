7b101adcd43a6f4b2d431b5e55814b671947c46a
Zowie <zowielangdon@gmail.com>
2015-07-13 16:03:55 +0200

    Bump version and add README.md

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ else:
 
 setup(
     name='drf-nested-decorator',
-    version='0.2',
+    version='0.3',
     author=u'Zowie Langdon',
     author_email='zowie@akoten.com',
     packages=['drf_nested_decorator'],
